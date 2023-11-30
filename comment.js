955d4992f8c861b5c70d5f42273764f33c81d7c7
Isaac López <isaaclovi@gmail.com>
2023-11-30 01:30:15 +0000

    Copilot Third commit

--- comment.js
+++ comment.js
@@ -1,12 +1,33 @@
 // Create web server
-// 1. npm install express
-// 2. npm install body-parser
+// Date: 2021-01-26
+// Creator: Nhat Hoang
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-app.use(bodyParser.urlencoded({extended:false}));
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
-// Create web server
-app.listen(3000, function(){
-    console.log('Server running at http://
+// Import controller
+const commentController = require(path.join(__dirname, '..', 'controllers', 'commentController.js'));
+
+// Get all comments
+router.get('/', commentController.getAllComments);
+
+// Get all comments of a post
+router.get('/post/:postID', commentController.getAllCommentsOfPost);
+
+// Get all comments of a user
+router.get('/user/:userID', commentController.getAllCommentsOfUser);
+
+// Get a comment by ID
+router.get('/:commentID', commentController.getCommentByID);
+
+// Create a comment
+router.post('/', commentController.createComment);
+
+// Update a comment
+router.put('/:commentID', commentController.updateComment);
+
+// Delete a comment
+router.delete('/:commentID', commentController.deleteComment);
+
+module.exports = router;
